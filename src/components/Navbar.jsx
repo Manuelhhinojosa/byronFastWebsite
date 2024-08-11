620273102aedff32054577a9d67e33cf0cab1f3d
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-11 18:41:59 -0400

    Styled Navbar logos with tailwindCSS Added background color to App component using Snippets for a gradient bg color design Completed stying of Navbar component. Next step: Hero component.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <div className="text-white">BF</div>
+        <div className="text-white mx-2 w-10">BF</div>
       </div>
-      <div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
         <FaSquareXTwitter />
         <FaInstagram />
