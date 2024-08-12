54bb529933b34e2f524608df2f5c86d1919b8b13
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-12 09:59:29 -0400

    Added abut Component in components dir and imported it into App component. Added about img to Images dir in assets dir. Completed About component with three different sections. Header, img and about text. Styled component with tailwindCSS. Fixed bug in App component that prevented background color to be rendered correctly. Next: skills component.

---
+++ src/components/About.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+//Images
+import aboutImg from "../assets/images/aboutImg.jpeg";
+
+const About = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h2 className="my-20 text-center text-4xl">
+        About <span className="text-neutral-500">Me</span>
+      </h2>
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex items-center justify-center">
+            <img className="rounded-2xl" src={aboutImg} alt="aboutImg" />
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2">
+          <div className="flex justify-center lg:justify-start">
+            <p className="my-2 max-w-xl py6 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In illo
+              similique, earum impedit amet temporibus vero, doloribus, delectus
+              vel alias magni sit nostrum tenetur perferendis blanditiis
+              necessitatibus saepe et quisquam. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Quae hic atque quas reprehenderit
+              reiciendis veritatis vero quaerat magni, excepturi dolores,
+              adipisci officia velit expedita repellat. Blanditiis nisi quis
+              aliquam distinctio? Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Debitis, qui? Cupiditate rerum doloremque eius
+              molestias dolore nostrum, magni odio quisquam consequatur.
+              Impedit, excepturi? Repellendus consectetur est maxime numquam!
+              Animi, labore.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,22 +15,17 @@ const Hero = () => {
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
               Content Creator
             </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter text-justify">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque,
               nobis libero harum eos vel enim voluptas possimus dolores dolore
               aperiam at dolorem mollitia ipsam dicta in ut tenetur consectetur
-              non? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Officiis, soluta labore voluptate earum perspiciatis ducimus animi
-              itaque minus suscipit voluptates id illo culpa ratione quia
-              officia ut fugiat voluptatem iure. Lorem ipsum dolor sit, amet
-              consectetur adipisicing elit. Saepe placeat voluptas similique
-              repellendus ipsam minima assumenda unde?.
+              non.
             </p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilePic} alt="profile-pic" />
+            <img className="rounded-2xl" src={profilePic} alt="profile-pic" />
           </div>
         </div>
       </div>
