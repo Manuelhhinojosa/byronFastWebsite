99a1b9b914a6eed6a434961e6e31a668f96c9de3
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-12 09:31:49 -0400

    Added actual client’s profile pic to images dir in assets dir. Actual image of client is now rendered.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,10 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-light lg:mt-16 lg:text-8xl">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
               Byron Fast
             </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-light text-transparent">
+            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
               Content Creator
             </span>
             <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
