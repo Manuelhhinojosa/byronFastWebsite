386ac5866ca711129c7ef0f733c944149d2639c6
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-12 13:05:22 -0400

    removed unnecessary icons as per client's request from Navbar component. Adjusted size of Navbar logo as per client's request.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 // React icons
 import { FaLinkedin } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa";
 
 // images
 
@@ -11,12 +9,12 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <div className="text-white mx-2 w-10">BF</div>
+        <div className="text-white mx-2 w-10 text-5xl">BF</div>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaSquareXTwitter />
-        <FaInstagram />
+        <a href="https://www.linkedin.com/in/manuelhh/">
+          <FaLinkedin />
+        </a>
       </div>
     </nav>
   );
