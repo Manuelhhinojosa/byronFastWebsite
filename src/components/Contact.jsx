fbc38bd2acaf400674ca785273aabd11c39a2850
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-12 14:58:47 -0400

    Imported framer motion library to Contact component to animate the elements there. Added animation to containers and elements Next: correct text and add correct text and images. waiting to get info from client.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 
+// libraries
+import { motion } from "framer-motion";
+
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
-      <h2 className="my-10 text-center text-4xl">Get in touch</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl"
+      >
+        Get in touch
+      </motion.h2>
       <div className="text-center tracking-tighter">
-        <p className="my-4">Toronto, Ontario. Canada.</p>
-        <p className="my-4">416-821-6982</p>
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          Toronto, Ontario. Canada.
+        </motion.p>
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          416-821-6982
+        </motion.p>
         <p className="my-4 ">byron.fast@gmail.com</p>
       </div>
     </div>
