184887edf0377a0101722059a29f4181bf74e28e
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-12 12:30:12 -0400

    Added Contact component to components dir. exported it to App component. Created containers for contact info. Styled using tailwindCSS.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import { Experience } from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Skills />
         <Experience />
         <Projects />
+        <Contact />
       </div>
     </div>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-20">
+      <h2 className="my-10 text-center text-4xl">Get in touch</h2>
+      <div className="text-center tracking-tighter">
+        <p className="my-4">Toronto, Ontario. Canada.</p>
+        <p className="my-4">416-821-6982</p>
+        <p className="my-4 ">byron.fast@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
