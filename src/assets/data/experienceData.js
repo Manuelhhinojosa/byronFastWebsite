a4db0c178555c3e21dc3d61da82dd0748ef18267
Manuelhhinojosa <mangud.hinojosa@gmail.com>
2024-08-14 14:12:10 -0400

    Updated experice.js file with the actual data provided by the client. It is now rendering in the Experience component. Next: waiting for client’s last pieces of data to be provided (links to his work to be added inside the projects component for each of the projects rendered, and finally deployment.

--- src/assets/data/experienceData.js
+++ src/assets/data/experienceData.js
@@ -1,38 +1,30 @@
 const experienceData = [
   {
-    year: "9999 - Present",
-    role: "Teacher shalala",
-    company: "Dunno",
+    year: "April 2021 - Present",
+    role: "Content Writer",
+    company: "Factinate",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. In ill similique, earum impedit amet temporibus vero, doloribus, delectus vel alias magni sit nostrum tenetur perferendis blanditiis necessitatibus saepe et quisquam. Lorem ipsum dolor sit ametconsectetur adipisicing elit.",
+      "As a content writer at Fractionate over the last 3 years, I crafted compelling and SEO-driven content that effectively communicated complex ideas to diverse audiences. My role involved researching industry trends, creating engaging blog posts, developing marketing copy, and refining brand messaging to enhance online visibility and drive audience engagement.",
   },
   {
-    year: "9999 - Present",
-    role: "Teacher shalala",
-    company: "Dunno",
+    year: "March 2021 - Present",
+    role: "Content Writer",
+    company: "Creative Touch Marketing",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. In ill similique, earum impedit amet temporibus vero, doloribus, delectus vel alias magni sit nostrum tenetur perferendis blanditiis necessitatibus saepe et quisquam. Lorem ipsum dolor sit ametconsectetur adipisicing elit.",
+      "As a Content Writer at Creative Touch Marketing for the past 3 years, I crafted engaging and persuasive content for diverse clients, including blog posts, website copy, and marketing materials. My role involved research, SEO optimization, and tailoring content to enhance brand voice and drive audience engagement.",
   },
   {
-    year: "9999 - Present",
-    role: "Teacher shalala",
-    company: "Dunno",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. In ill similique, earum impedit amet temporibus vero, doloribus, delectus vel alias magni sit nostrum tenetur perferendis blanditiis necessitatibus saepe et quisquam. Lorem ipsum dolor sit ametconsectetur adipisicing elit.",
+    year: "July 2005 - Present",
+    role: "English Teacher",
+    company: "CES Toronto (Centre of English Studies Toronto)",
+    description: "Cambridge Classes, Business Classes, General English, IELTS",
   },
   {
-    year: "9999 - Present",
-    role: "Teacher shalala",
-    company: "Dunno",
+    year: "January 2008 - January 2011",
+    role: "Writer",
+    company: "Buck Productions",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. In ill similique, earum impedit amet temporibus vero, doloribus, delectus vel alias magni sit nostrum tenetur perferendis blanditiis necessitatibus saepe et quisquam. Lorem ipsum dolor sit ametconsectetur adipisicing elit.",
-  },
-  {
-    year: "9999 - Present",
-    role: "Teacher shalala",
-    company: "Dunno",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. In ill similique, earum impedit amet temporibus vero, doloribus, delectus vel alias magni sit nostrum tenetur perferendis blanditiis necessitatibus saepe et quisquam. Lorem ipsum dolor sit ametconsectetur adipisicing elit.",
+      "As a Writer at Buck Productions, I delivered scripts for two TV shows: Rich Bride Poor Bride and Rich Groom Poor Groom. My role involved creating compelling narratives and dialogue, ensuring content met production standards and engaged viewers effectively.",
   },
 ];
 
